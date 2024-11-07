7035d1d893ca9f53323977eec5765ec167e3f43d
Collin <collinmorudi@gmail.com>
2024-11-07 18:10:22 +0200

    added logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 """
-This module contains a function to obfuscate sensitive fields in log messages.
+This module contains functions and classes to obfuscate sensitive
+fields in log messages.
 """
 
+
+import logging
 import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
     Obfuscates sensitive information in a log message by replacing specified
     field values with a redaction string.
@@ -23,3 +27,38 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """
     pattern = f"({'|'.join(fields)})=([^ {separator}]+)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class to filter sensitive information
+    in log records. """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initializes the formatter with specific fields to redact.
+
+        Args:
+            fields (List[str]): List of fields to be redacted in the
+            log message.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the log record, applying redaction to specified fields.
+
+        Args:
+            record (logging.LogRecord): The log record to be formatted.
+
+        Returns:
+            str: The formatted and redacted log message.
+        """
+        record.msg = (
+          filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        )
+        return super().format(record)
